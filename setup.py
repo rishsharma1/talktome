7b29ce45399b580d12fcdb84185acc55339d8631
Rishabh Sharma <rishsharma13@gmail.com>
2017-05-10 19:25:34 +1000

    yak shaving

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': '',
+    'author': 'Rishabh Sharma',
+    'url': '',
+    'download_url': '',
+    'author_email': 'rishsharma13@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['talktome'],
+    'scripts': [],
+    'name': 'talktome'
+}
+
+setup(**config)
---
+++ tests/talktome_tests.py
@@ -0,0 +1,8 @@
+from nose.tools import *
+import talktome
+
+def setup():
+    print "SETUP!"
+
+def test_basic():
+    print "I RAN!"
